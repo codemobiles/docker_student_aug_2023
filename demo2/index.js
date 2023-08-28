4ed0d53796ba19bcd969fd2634db560ba5fc4401
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-28 13:38:37 +0700

    1234

--- demo2/index.js
+++ demo2/index.js
@@ -1,12 +1,15 @@
-const express=require("express")
-const app=express()
+const express = require("express");
+const app = express();
 
-app.get("/", (req, res)=>{
-  res.json({result: 'ok'})
-})
+app.get("/", (req, res) => {
+  res.json({ result: "ok" });
+});
 
+// http://localhost:3000/register?username=lek&password=6666
+app.get("/register", (req, res) => {
+  res.json({ result: "register ok", detail: req.query });
+});
 
-app.listen(process.env.PORT || 3000, ()=>{
-  console.log(`Servier is running port ${process.env.PORT}`)
-})
-
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Servier is running port ${process.env.PORT}`);
+});
