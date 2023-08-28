5ce62668b69f87fc3a6b1dd7ada0df716287e8b5
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-28 11:28:23 +0700

    1234

--- demo1/index.js
+++ demo1/index.js
@@ -1,2 +1,16 @@
 // docker run -v ./:/app --rm --name nodeapp -it node node /app/index.js
 console.log("Hey");
+
+const main = async () => {
+  console.log("Running..");
+};
+
+// Run the main function in an infinite loop
+const runLoop = async () => {
+  while (true) {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    await main();
+  }
+};
+
+runLoop();
