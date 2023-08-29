6e2c793e7c1b34a2cd5db20da03f1d701a6830d7
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-29 13:26:49 +0700

    1234

--- demo7/frontend/src/App.tsx
+++ demo7/frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 export default function App() {
   const [dataArray, setdataArray] = useState([]);
+  const [newData, setNewData] = useState("");
   useEffect(() => {
     loadData();
   });
@@ -11,8 +12,26 @@ export default function App() {
     setdataArray(await response.json());
   };
 
+  const addData = async (data: string) => {
+    await fetch("http://localhost:8080/add/" + data);
+  };
+
   return (
-    <div>
+    <div style={{ padding: 20 }}>
+      <input
+        type="text"
+        placeholder="Enter your data"
+        value={newData}
+        onChange={(event) => setNewData(event.target.value)}
+      />
+      <button
+        onClick={() => {
+          addData(newData);
+          setNewData("");
+        }}
+      >
+        Add
+      </button>
       <ul>
         {dataArray.map((e: { _id: string; name: string }) => (
           <li key={e._id}>{e.name}</li>
